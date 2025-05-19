775954d5f23215fab6dfeba0ae97ac21ca7b6b1e
Adrian Faist <faist@synyx.de>
2025-05-19 10:47:25 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    {{ text }}
+    <ul>
+      <li class="box" v-for="metric in metrics">
+        {{ metric.value }}
+      </li>
+    </ul>
+  </div>
+  <div>
+    <form class="einstellungen" @submit.prevent="onFormSubmit">
+      <div>
+        <h2>Wähle eine Option:</h2>
+        <label>
+          <input type="radio" value="hochzählen" v-model="selectedOption" />
+          hochzählen
+        </label>
+        <label>
+          <input type="radio" value="runterzählen" v-model="selectedOption" />
+          runterzählen
+        </label>
+      </div>
+      <div>
+        <input type="text" v-model="input" name="address" rows="5" cols="30" style="resize: none"/>
+      </div>
+      <button type="submit" label="Submit">
+        text
+      </button>
+    </form>
+  </div>
+</template>
+<script>
+import {ref} from 'vue'
+
+const text = ref()
+
+export default {
+  data() {
+    return {
+      text: "erstellte metriken:",
+      input: "null",
+      metrics: []
+    }
+  },
+  methods: {
+    onFormSubmit() {
+      let newMetric = {
+        value: this.input
+      }
+      this.metrics.push(newMetric)
+      this.input = null
+    }
+  }
+}
+</script>
+
+<style scoped>
+.einstellungen, .box {
+  border: 2px solid #ccc;
+  border-radius: 8px;
+  padding: 1rem;
+  max-width: 300px;
+  background-color: black;
+  display: block;
+  margin: 0.5rem;
+}
+</style>
